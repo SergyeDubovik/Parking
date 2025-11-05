f5d45c105a68069ff20ba7dd7be0601716116b4d
serg <supermirko9@gmail.com>
2025-11-05 22:08:07 +0200

    refactored interface structure

--- src/com/core/Persistable.java
+++ src/com/core/Persistable.java
@@ -2,7 +2,7 @@ package com.parking.src.com.core;
 
 import java.io.IOException;
 
-public interface ParkingData {
+public interface Persistable {
     void saveData() throws IOException;
     void loadData() throws IOException;
 }
